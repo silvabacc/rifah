a32a48f831da48f17ec3b8f6b66bf7225f737f18
silvabacc <bavernaz@hotmail.com>
2025-05-02 21:00:42 +0100

    add uuid to duas and modal

--- src/SavedDuas.tsx
+++ src/SavedDuas.tsx
@@ -1,34 +1,52 @@
 import { useEffect, useState } from "react";
 import { useLocalStorage } from "./hooks/useLocalStorage";
 import { SavedDua } from "./types";
-import { Card } from "antd";
 import Masonry from "react-masonry-css";
 import "./SavedDuas.css";
 import DuaCard from "./components/DuaCard";
+import { Divider, Modal } from "antd";
 
 const SavedDuas = () => {
   const { getSavedDua } = useLocalStorage();
+  const [duaSelectedId, setDuaSelectedId] = useState<string>();
+  const [modalOpen, setModalOpen] = useState(false);
   const [cards, setCards] = useState<SavedDua[]>([]);
+
+  const duaSelected = cards.find((card) => duaSelectedId === card.dua.id)
 
   useEffect(() => {
     const savedDuas = getSavedDua();
     setCards(savedDuas);
   }, []);
 
+  const onCardClick = (duaId: string) => {
+    setModalOpen(true);
+    setDuaSelectedId(duaId)
+  }
+
   return (
     <Masonry
       className="my-masonry-grid"
       columnClassName="my-masonry-grid_column"
       breakpointCols={3}
-    >
+      >
       {cards.map((card, index) => (
         <div
-          className="rounded border border-transparent group transition-all duration-300"
-          key={`${card.duaName}-${index}`}
+        onClick={() => onCardClick(card.dua.id)}
+        className="rounded bord er border-transparent group transition-all duration-300"
+        key={`${card.duaName}-${index}`}
         >
           <DuaCard title={card.duaName} dua={card.dua} />
         </div>
       ))}
+      <Modal title={duaSelected?.duaName} open={modalOpen} closable onCancel={() => setModalOpen(false)}>
+      <Divider/>
+      <div className="flex justify-center flex-col text-center" >
+        <p>{duaSelected?.dua.arabic}</p>
+        <Divider/>
+        <p>{duaSelected?.dua.translation}</p>
+      </div>
+      </Modal>
     </Masonry>
   );
 };
--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,14 +1,16 @@
 import { Dua, SavedDua } from "../types";
+import { v4 as uuidv4 } from "uuid";
 
 export const useLocalStorage = () => {
   const saveDua = (duaName: string, duas: Dua[]) => {
     const savedDuas = getSavedDua();
+    const id = uuidv4();
 
     const combinedDua = duas.reduce(
       (acc, curr) => {
         return {
           title: duaName,
-          id: "",
+          id: id,
           latin: curr.latin.concat(" ", acc.latin),
           arabic: curr.arabic.concat(" ", acc.arabic),
           translation: curr.translation.concat(" ", acc.translation),
